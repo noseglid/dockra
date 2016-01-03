ee70d8b9dfb919ebba32ecbc0d23b7bb7dd7e243
Alexander Olsson <noseglid@gmail.com>
2016-01-03 22:43:48 +0100

    Better create layout

--- src/config.js
+++ src/config.js
@@ -1,6 +1,7 @@
 export default {
   docker: {
+    registry: 'https://registry.hub.docker.com',
     host: 'tcp://192.168.99.101:2376',
     certPath: '/Users/noseglid/.docker/machine/machines/dockra'
-  }
+  },
 };
--- src/containers/create/create.js
+++ src/containers/create/create.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import LinkedStateMixin from 'react-addons-linked-state-mixin';
+import update from 'react-addons-update';
 import NestLinkedStateMixin from 'react-nest-link-state';
 import format from '../../lib/format';
 import docker from '../../lib/docker';
@@ -10,31 +11,43 @@ export default React.createClass({
 
   getInitialState() {
     return {
-      image: docker.getImage(this.props.params.imageId),
-      imageName: format.hash(this.props.params.imageId),
-      env: ['KEY1=VALUE1', 'KEY2=VALUE2']
+      container: {
+        Env: [],
+        Tty: false,
+        Image: this.props.params.imageId
+      }
     };
   },
 
   componentDidMount() {
-    this.state.image.inspectAsync()
+    docker.getImage(this.state.container.Image).inspectAsync()
       .then(i => {
         const repoTag = i.RepoTags.find(t => !t.match(/[0-9a-f]{64}/));
         if (!repoTag) return;
-        this.setState({ imageName: repoTag });
+
+        this.setState({
+          container: update(this.state.container, { Image: { $set: repoTag } })
+        });
       });
   },
 
+  removeEnv(index) {
+    const container = this.state.container;
+    container.Env.splice(index, 1);
+    this.setState({ container: container });
+  },
+
+  addEnv() {
+    const container = this.state.container;
+    container.Env = container.Env.concat('');
+    this.setState({ container: container });
+  },
+
   createContainer() {
-    const opts = {
-      name: this.state.name,
-      Cmd: this.state.cmd.split(' '),
-      Image: this.state.imageName,
-      Hostname: this.state.hostname,
-      Tty: false,
-      Env: this.state.env
-    };
-    docker.createContainer(opts)
+    const createOpts = update(this.state.container, {
+      Cmd: { $set: this.state.container.Cmd.split(' ') }
+    });
+    docker.createContainer(createOpts)
       .then(container => {
         let promise = Promise.resolve();
         if (this.state.startOnCreated) {
@@ -54,27 +67,34 @@ export default React.createClass({
   render() {
     return (
       <div className="container">
-        <h1>Create container from <code>{this.state.imageName}</code></h1>
+        <h1>Create container from <code>{this.state.container.Image}</code></h1>
         <form className="form-horizontal">
 
           <div className="form-group">
             <label htmlFor="input-name" className="col-sm-2 control-label">Name</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" id="input-name" valueLink={this.linkState('name')} placeholder="Container name (default: <generated>)" />
+              <input type="text" className="form-control" id="input-name" valueLink={this.nestLinkedState([ 'container', 'name' ])} placeholder="Container name (default: <generated>)" />
             </div>
           </div>
 
           <div className="form-group">
             <label htmlFor="input-hostname" className="col-sm-2 control-label">Hostname</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" id="input-hostname" valueLink={this.linkState('hostname')} placeholder="Container hostname (default: container id)" />
+              <input type="text" className="form-control" id="input-hostname" valueLink={this.nestLinkedState([ 'container', 'Hostname' ])} placeholder="Container hostname (default: container id)" />
             </div>
           </div>
 
           <div className="form-group">
             <label htmlFor="input-cmd" className="col-sm-2 control-label">Command</label>
             <div className="col-sm-10">
-              <input type="text" className="form-control" id="input-cmd" valueLink={this.linkState('cmd')} placeholder="Command to run" />
+              <input type="text" className="form-control" id="input-cmd" valueLink={this.nestLinkedState([ 'container', 'Cmd' ])} placeholder="Command to run" />
+            </div>
+          </div>
+
+          <div className="form-group">
+            <label htmlFor="input-wd" className="col-sm-2 control-label">Working dir</label>
+            <div className="col-sm-10">
+              <input type="text" className="form-control" id="input-wd" valueLink={this.nestLinkedState([ 'container', 'WorkingDir' ])} placeholder="Working directory for command" />
             </div>
           </div>
 
@@ -87,18 +107,31 @@ export default React.createClass({
           </div>
 
           {
-            this.state.env.map((entry, index) => {
+            this.state.container.Env.map((entry, index) => {
               return (
                 <div key={index} className="form-group">
-                  <label htmlFor="input-cmd" className="col-sm-2 control-label">Environment</label>
-                  <div className="col-sm-10">
-                    <input type="text" className="form-control" id="input-cmd" valueLink={this.nestLinkedState(['env', index])} placeholder="KEY=VALUE" />
+                  <label htmlFor="input-env-{$index}" className="col-sm-2 control-label">Environment</label>
+                  <div className="col-sm-9">
+                    <input type="text" className="form-control" id="input-env-${index}" valueLink={this.nestLinkedState(['container', 'Env', index])} placeholder="KEY=VALUE" />
+                  </div>
+                  <div className="col-sm-1">
+                    <button className="form-control btn btn-default btn-xs" onClick={this.removeEnv.bind(this, index)}>
+                      <span className="glyphicon glyphicon-trash"></span>
+                    </button>
                   </div>
                 </div>
               );
             })
           }
+          <div className="form-group">
+            <div className="col-sm-offset-2 col-sm-3">
+              <button className="form-control btn btn-default btn-xs" onClick={this.addEnv}>
+                <span className="glyphicon glyphicon-plus"></span> Add environment
+              </button>
+            </div>
+          </div>
 
+          <hr />
           <div className="form-group">
             <div className="col-sm-offset-2 col-sm-10">
               <button type="submit" className="btn btn-primary" onClick={this.createContainer}>Create</button>
